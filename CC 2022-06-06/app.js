3ab77c0485ff8de516a6e5ffa04e258e71a62b97
Jordan Veloso <jrveloso96@gmail.com>
2022-06-06 10:53:27 -0500

    Coding challenge and solution for 06-06-2022

---
+++ CC 2022-06-06/app.js
@@ -0,0 +1,18 @@
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+//P(arameters)
+//  num
+//R(eturns)
+//  string version of num
+//E(xamples)
+//  123 --> "123"
+//  999 --> "999"
+//P(seudo Code)
+//  1.) When num is entered into function
+//  2.) Convert to string
+//  3.) Return string
+
+const numberToString = num => String(num)
+

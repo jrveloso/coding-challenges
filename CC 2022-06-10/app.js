13e4a2e5aa1aeb85b37b909e52456ab3a3bc2230
Jordan Veloso <jrveloso96@gmail.com>
2022-06-10 10:20:34 -0500

    Coding challenge and solution for 06-10-2022.

---
+++ CC 2022-06-10/app.js
@@ -0,0 +1,13 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+//P(arameters)
+//  i
+//R(eturns)
+//  return i doubled
+//E(xamples)
+//  
+//P(seudo Code)
+//  1.) When i is entered in function
+//  2.) Double value of i and return it
+
+const doubleInteger = i => i *= 2

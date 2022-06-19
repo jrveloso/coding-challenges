bfc9cf0f6264aa885884754db56f462ce10f3a69
Jordan Veloso <jrveloso96@gmail.com>
2022-06-19 12:49:29 -0500

    Coding challenge and solution for 06-19-2022.

---
+++ CC 2022-06-19/main.js
@@ -0,0 +1,48 @@
+// There is a bus moving in the city, and it takes and drop some people in each bus stop.
+
+// You are provided with a list (or array) of integer pairs. Elements of each pair represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+
+// Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+
+// The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+
+//P(arameters)
+//  array
+//R(eturns)
+//  ridersLeft
+//E(xamples)
+//  
+//P(seudo Code)
+//  1.) When a user provides an array
+//  2.) Remove second number in each array and store in new array
+//  3.) Add up first value arrays and add values in new array
+//  4.) Subtract total of new array sum from first value array
+//  5.) Return value
+
+function number(busStops) {
+    let ridersLeft = []
+ 
+    for(let i = 0; i <= busStops.length - 1; i++) {
+         ridersLeft[i] = busStops[i].pop()
+    }
+    let ridersBoarding = busStops.reduce((sum, curr) => Number(sum) + Number(curr), 0)
+    ridersLeft = ridersLeft.reduce((sum, curr) => sum + curr, 0)
+
+    return ridersBoarding - ridersLeft
+}
+
+// let number = function(busStops){
+//     let ridersLeft = []
+ 
+//     for(let i = 0; i <= busStops.length - 1; i++) {
+//          ridersLeft[i] = busStops[i].pop()
+//     }
+//     let ridersBoarding = busStops.reduce((sum, curr) => Number(sum) + Number(curr), 0)
+//     ridersLeft = ridersLeft.reduce((sum, curr) => sum + curr, 0)
+
+//     return ridersBoarding - ridersLeft
+// }

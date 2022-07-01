f35d60bd16818180976496013675db5247eea84b
Jordan Veloso <jrveloso96@gmail.com>
2022-07-01 08:11:19 -0500

    Coding challenge and solution for 07-01-2022.

---
+++ CC 2022-07-01/main.js
@@ -0,0 +1,15 @@
+// Complete the solution so that it reverses the string passed into it.
+
+//P(arameters)
+//  string
+//R(eturns)
+//  new string reversed
+//E(xamples)
+//  'world'  =>  'dlrow'
+//  'word'   =>  'drow'
+//P(seudo Code)
+//  1.) When a string is entered
+//  2.) Reverse string
+//  3.) Return it
+
+const solution = str => str.split("").reverse().join("")

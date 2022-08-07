b2352b7cbf3017879f940e9edb324895c85b8113
Jordan Veloso <jrveloso96@gmail.com>
2022-08-07 11:40:59 -0500

    Coding challenge and solution for 08-07-2022.

---
+++ CC 2022-08-07/main.js
@@ -0,0 +1,15 @@
+// Write a function to split a string and convert it into an array of words.
+
+//P(arameters)
+//   string
+//R(eturns)
+//  array
+//E(xamples)
+//  "Robin Singh" ==> ["Robin", "Singh"]
+//  "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+//P(seudo Code)
+//  1.) When string is given
+//  2.) split string by " "
+//  3.) return array of words
+
+const stringToArray = string => string.split(" ")

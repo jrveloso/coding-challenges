757169a858452d3874c9c52e8f1a3a83083a4d10
Jordan Veloso <jrveloso96@gmail.com>
2022-08-30 09:34:50 -0500

    Coding challenge and solution for 08-30-2022.

---
+++ CC 2022-08-30/main.js
@@ -0,0 +1,15 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+//P(arameters)
+//  string
+//R(eturns)
+//  string without " "
+//E(xamples)
+//  
+//P(seudo Code)
+//  1.) When given a string
+//  2.) Split string into array
+//  3.) Filter every character that's not a " "
+//  4.) Join array and return 
+
+const noSpace = x => x.split("").filter(element => element !== " ").join("")

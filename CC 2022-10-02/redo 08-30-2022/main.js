231a366161f582eb5e4a60a6fc74a04f8c18db3d
Jordan Veloso <jrveloso96@gmail.com>
2022-08-30 08:45:53 -0500

    Redo coding challenge from 08-30-2022.

---
+++ CC 2022-10-02/redo 08-30-2022/main.js
@@ -0,0 +1,16 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+//P(arameters)
+//  string
+//R(eturns)
+//  string without " "
+//E(xamples)
+//  
+//P(seudo Code)
+//  1.) When given a string
+//  2.) Split string into array
+//  3.) Filter every character that's not a " "
+//  4.) Join array and return 
+
+const noSpace = str => str.split(" ").join("")
+console.log(noSpace("hi there you are great"))

b0f4dec96fde598819f237595a6ca1c69e1f981c
Jordan Veloso <jrveloso96@gmail.com>
2022-09-03 10:22:38 -0500

    Coding challenge and solution 09-03-2022.

---
+++ CC 2022-09-03/main.js
@@ -0,0 +1,17 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
+//P(arameters)
+//  array a, and value x
+//R(eturns)
+//  true or false
+//E(xamples)
+//  
+//P(seudo Code)
+//  When given array and x
+//  If array holds x
+//  Return true
+//  Else return false
+
+const check = (a, x) => a.includes(x)

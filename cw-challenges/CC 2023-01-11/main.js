662d99649ea691cd20b4167989d7425fe5becc5d
Jordan Veloso <jrveloso96@gmail.com>
2023-01-11 20:54:34 -0600

    Coding challenge and solution 01-11-2023.

--- cw-challenges/CC 2023-01-11/main.js
+++ cw-challenges/CC 2023-01-11/main.js
@@ -14,10 +14,10 @@
 // Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
-function removeDuplicates(arr) {
-    //filter -> check for duplicates
+function removeDupes(arr) {
+    //filter -> check for dupes
     return arr.filter((num, i, a) => i === a.lastIndexOf(num))
 }
 
-console.log(removeDuplicates([1,1,2]), [1,2])
-console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]), [0,1,2,3,4])
+console.log(removeDupes([1,1,2]), [1,2])
+console.log(removeDupes([0,0,1,1,1,2,2,3,3,4]), [0,1,2,3,4])

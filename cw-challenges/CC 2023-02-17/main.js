822b90e3af0cd6f5c2217983da41a7f639997d5e
Jordan Veloso <jrveloso96@gmail.com>
2023-02-17 17:19:00 -0600

    Coding challenge and solution 02-17-2023.

---
+++ cw-challenges/CC 2023-02-17/main.js
@@ -0,0 +1,26 @@
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+// Example 1:
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+
+// Example 2:
+
+// Input: root = [1,null,2]
+// Output: 2
+
+// recursive solution
+// issues of repeating routes as tree grows
+
+// movie theater solution
+// you can't see so you ask someone in front of you how many rows are in front
+// they ask and so on until the end
+
+const maxDepth = function(root) {
+    if(!root) return 0
+    // Add one 
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+}

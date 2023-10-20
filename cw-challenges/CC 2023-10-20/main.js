6009c6acea5648a1a23583a48629e395e10461b7
Jordan Veloso <jrveloso96@gmail.com>
2023-10-20 16:45:15 -0500

    Coding challenge and solution 10-20-2023.

---
+++ cw-challenges/CC 2023-10-20/main.js
@@ -0,0 +1,20 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+// assert.strictEqual(sumMix([9, 3, '7', '3']), 22);
+// assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42);
+// assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41);
+
+// arr -> str & num
+// num -> sum
+
+function sumAllValues(arr) {
+  // sum all of the nums in arr
+  // loop through nums and add together
+  // reduce -> convert to nums and sum
+  return arr.reduce((a, c) => a + Number(c), 0);
+}
+
+console.log(sumAllValues([9, 3, "7", "3"]), 22);
+console.log(sumAllValues(["5", "0", 9, 3, 2, 1, "9", 6, 7]), 42);

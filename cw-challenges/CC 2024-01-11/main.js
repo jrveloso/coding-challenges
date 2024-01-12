b670baa3e23efde827304c25fb1e87aa613cb37c
Jordan Veloso <jrveloso96@gmail.com>
2024-01-11 19:14:26 -0600

    update to coding challenge 01-11-2024.

--- cw-challenges/CC 2024-01-11/main.js
+++ cw-challenges/CC 2024-01-11/main.js
@@ -13,5 +13,15 @@
 // isDigit("zero")
 
 function isDigit(s) {
-  return Number(s) ? true : false
+  return parseFloat(s) === Number(s)
 }
+
+console.log(isDigit("3"), true)
+console.log(isDigit("  3  "), true)
+console.log(isDigit("-3.23"), true)
+console.log(isDigit("-234.4"), true)
+console.log(isDigit("s2324"), false)
+console.log(isDigit("3-4"), false)
+console.log(isDigit("  3   5"), false)
+console.log(isDigit("3 5"), false)
+console.log(isDigit("zero"), false)

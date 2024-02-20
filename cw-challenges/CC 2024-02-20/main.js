5cece65f58fbc862e86c1f2c9aee18d85464c788
Jordan Veloso <jrveloso96@gmail.com>
2024-02-20 17:45:12 -0600

    Coding challenge and solution 02-20-2024.

---
+++ cw-challenges/CC 2024-02-20/main.js
@@ -0,0 +1,36 @@
+// A stream of data is received and needs to be reversed.
+
+// Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+// 11111111  00000000  00001111  10101010
+//  (byte1)   (byte2)   (byte3)   (byte4)
+// should become:
+
+// 10101010  00001111  00000000  11111111
+//  (byte4)   (byte3)   (byte2)   (byte1)
+// The total number of bits will always be a multiple of 8.
+
+// The data is given in an array as such:
+
+// [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+
+// create new array of bytes in groups of 8
+// reverse
+
+function dataReverse(data) {
+    let bytes = Array(data.length / 8);
+//   console.log(bytes);
+    let j = 1
+    for (let i = bytes.length - 1; i > -1 ; i--) {
+        bytes[i] = data.splice(0, 8)
+        j += 1
+    }
+    return bytes.flat()
+}
+
+console.log(
+  dataReverse([
+    1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+    0, 1, 0, 1, 0, 1, 0,
+  ])
+);

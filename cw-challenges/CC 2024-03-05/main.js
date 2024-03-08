ea6b4ebfb32c01e710b91a5be9e203077e5bc3f4
Jordan Veloso <jrveloso96@gmail.com>
2024-03-08 09:45:48 -0600

    Coding challenge and solution 03-05-2024.

---
+++ cw-challenges/CC 2024-03-05/main.js
@@ -0,0 +1,8 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+const binToDec = bin => parseInt(bin, 2)
+
+console.log(binToDec('0'), 0)
+console.log(binToDec('1'), 1)
+console.log(binToDec('10'), 2)
+console.log(binToDec('1001001'), 73)

e668d59e9b57bfb5bd6af040b1480ff89f38461d
Jordan Veloso <jrveloso96@gmail.com>
2024-08-22 10:17:38 -0500

    Coding challenge and solution 08-22-2024.

---
+++ cw-challenges/CC 2024-08-22/main.js
@@ -0,0 +1,38 @@
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+// Example 2:
+
+// Input: n = 1
+// Output: ["()"] 
+
+// Constraints:
+
+// 1 <= n <= 8
+
+const generateParenthesis = (n) => {
+    let res = []
+
+    function dfs(openP, closeP, s) {
+        console.log(openP, closeP)
+        if(openP === closeP && openP + closeP === n * 2) {
+            res.push(s)
+            return
+        }
+        if(openP < n) {
+            dfs(openP + 1, closeP, s + "(")
+        }
+        if(closeP < openP) {
+            dfs(openP, closeP + 1, s + ")")
+        }
+    }
+    dfs(0,0, "")
+    return res
+};
+
+// console.log(generateParenthesis(1), ["()"])
+console.log(generateParenthesis(3), ["((()))","(()())","(())()","()(())","()()()"])
+

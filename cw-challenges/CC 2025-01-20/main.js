260faca2a71c7560399fcb7aa5a8f6a39340f0fc
Jordan Veloso <jrveloso96@gmail.com>
2025-01-20 16:18:55 -0600

    Coding challenge and solution 01-20-2025.

---
+++ cw-challenges/CC 2025-01-20/main.js
@@ -0,0 +1,19 @@
+// You will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+// array -> integer
+// loop array
+// if i !== 0
+// if nums[i] !== nums[i - 1]
+// if index of current same as lastIndexOf -> add to nonRepeats
+
+function repeats(nums) {
+  return nums
+    .filter((num) => nums.indexOf(num) === nums.lastIndexOf(num))
+    .reduce((a, c) => a + c, 0);
+}
+
+console.log(repeats([4, 5, 7, 5, 4, 8]), 15);
+console.log(repeats([1, 18, 5, 5, 1, 4]), 22);
+console.log(repeats([3, 4, 8, 1, 4, 1, 3, 9]), 17);
